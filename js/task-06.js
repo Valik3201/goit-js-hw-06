b4e99121c32a6fdfdc56fb6316a55b82ff1856c5
Valik <valik3201@gmail.com>
2023-11-01 19:56:02 +0100

    FIXED: `task-06`

--- js/task-06.js
+++ js/task-06.js
@@ -2,15 +2,15 @@ const textInput = document.querySelector("#validation-input");
 const maxLength = textInput.getAttribute("data-length");
 
 const validation = () => {
-  if (textInput.value.length > maxLength) {
-    textInput.classList.remove("valid");
-    textInput.classList.add("invalid");
+  if (textInput.value.length === parseInt(maxLength)) {
+    textInput.classList.remove("invalid");
+    textInput.classList.add("valid");
   } else if (textInput.value.length === 0) {
     textInput.classList.remove("invalid");
     textInput.classList.remove("valid");
   } else {
-    textInput.classList.remove("invalid");
-    textInput.classList.add("valid");
+    textInput.classList.remove("valid");
+    textInput.classList.add("invalid");
   }
 };
 
