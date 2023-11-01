232b2e43a502665dbd13310ef7c17ba3eff2b3a5
Valik <valik3201@gmail.com>
2023-11-01 15:19:26 +0100

    ADD: `tast-02`

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulItems = document.querySelector("#ingredients");
+
+const heading = document.createElement("h2");
+heading.textContent = "Ingredients:";
+ulItems.before(heading);
+
+ingredients.forEach((ingridient) => {
+  const item = document.createElement("li");
+  item.textContent = ingridient;
+  item.className = "item";
+  ulItems.append(item);
+});
