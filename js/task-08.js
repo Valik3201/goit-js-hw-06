ef92085566f641bf03c177a8e544d716979c0ea2
Valik <valik3201@gmail.com>
2023-11-01 17:26:45 +0100

    ADD: `task-08`

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email;
+  const password = form.elements.password;
+
+  if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  form.reset();
+}
